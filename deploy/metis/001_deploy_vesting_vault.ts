831acc84615c7b430cf4e311fb6838197246aae8
Rami Husami <husamirami@gmail.com>
2023-08-14 21:06:01 +0300

    save rMetis deployment

--- deploy/metis/001_deploy_vesting_vault.ts
+++ deploy/metis/001_deploy_vesting_vault.ts
@@ -1,11 +1,13 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import {ABI, DeployFunction} from 'hardhat-deploy/types';
 import {StandardMerkleTree} from '@openzeppelin/merkle-tree';
+import {RMetis__factory, VestingVault} from '../../typechain-types';
 import fs from 'fs';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	const {deployer} = await hre.getNamedAccounts();
-	const {deploy} = hre.deployments;
+	const {ethers, getNamedAccounts, artifacts} = hre;
+	const {deployer} = await getNamedAccounts();
+	const {deploy, save} = hre.deployments;
 
 	const merkleTree = StandardMerkleTree.load(
 		JSON.parse(
@@ -15,6 +17,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 			)
 		)
 	);
+
 	const rootHash = merkleTree.root;
 	const {AIRDROP_DURATION_DAYS, REDEMPTION_START_DATE, REDEMPTION_END_DATE, MIN_PRICE, MAX_PRICE} = process.env;
 
@@ -23,6 +26,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 		args: [rootHash, AIRDROP_DURATION_DAYS, REDEMPTION_START_DATE, REDEMPTION_END_DATE, MIN_PRICE, MAX_PRICE],
 		log: true,
 		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+	});
+
+	// Save rMetis token
+	const vestingVaultContract = await ethers.getContract<VestingVault>('VestingVault', deployer);
+	const rMetisAddress = await vestingVaultContract.rMetis();
+	// const rMetisAddress = await ethers.getContractAt('RMetis', await vestingVaultContract.rMetis());
+
+	await save('RMetis', {
+		address: rMetisAddress,
+		abi: (await artifacts.readArtifact('RMetis')).abi,
 	});
 };
 export default func;
