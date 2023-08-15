c715095052d2dd2a60b104a915115b372680cb48
Rami Husami <husamirami@gmail.com>
2023-08-16 01:29:11 +0300

    deploy V1.0.0, revise deployment scripts

--- deploy/metis/001_deploy_vesting_vault.ts
+++ deploy/metis/001_deploy_vesting_vault.ts
@@ -1,8 +1,9 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {ABI, DeployFunction} from 'hardhat-deploy/types';
+import {DeployFunction} from 'hardhat-deploy/types';
 import {StandardMerkleTree} from '@openzeppelin/merkle-tree';
-import {RMetis__factory, VestingVault} from '../../typechain-types';
+import {VestingVault} from '../../typechain-types';
 import fs from 'fs';
+import {formatEther} from 'ethers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const {ethers, getNamedAccounts, artifacts} = hre;
@@ -12,13 +13,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const merkleTree = StandardMerkleTree.load(
 		JSON.parse(
 			fs.readFileSync(
-				'snapshots/merkle-bsc-0xe552Fb52a4F19e44ef5A967632DBc320B0820639-11170743-29591664.json',
+				// 'snapshots/merkle-bsc-0xe552Fb52a4F19e44ef5A967632DBc320B0820639-11170743-29591664.json',
+				'snapshots/merkle-dev-snapshot.json',
 				'utf-8'
 			)
 		)
 	);
 
 	const rootHash = merkleTree.root;
+
+	const sum = Array.from(merkleTree.entries()).reduce<bigint>((res, [i, val]) => {
+		return res + BigInt(val[1]);
+	}, BigInt(0));
+
+	console.log(`Airdrop size: ${formatEther(sum.toString())}`);
+
 	const {AIRDROP_DURATION_DAYS, REDEMPTION_START_DATE, REDEMPTION_END_DATE, MIN_PRICE, MAX_PRICE} = process.env;
 
 	await deploy('VestingVault', {
@@ -30,8 +39,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 	// Save rMetis token
 	const vestingVaultContract = await ethers.getContract<VestingVault>('VestingVault', deployer);
+
+	// Optional: deposit, most likely will be done from safe
+	// await vestingVaultContract.deposit({value: sum.toString(), gasLimit: 1000000});
+
 	const rMetisAddress = await vestingVaultContract.rMetis();
-	// const rMetisAddress = await ethers.getContractAt('RMetis', await vestingVaultContract.rMetis());
 
 	await save('RMetis', {
 		address: rMetisAddress,
